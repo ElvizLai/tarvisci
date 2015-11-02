7f19a3834e2498f08e89d1978bf4cc6ca45aa59c
ElvizLai <sdrzlyz@126.com>
2015-11-02 15:02:49 +0800

    init

---
+++ initial/initial.go
@@ -0,0 +1,12 @@
+/**
+ * Created by elvizlai on 2015/11/2 14:32
+ * Copyright © PubCloud
+ */
+package initial
+
+import (
+	"fmt"
+)
+func init() {
+	fmt.Println("initial...")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+/**
+ * Created by elvizlai on 2015/11/2 14:31
+ * Copyright © PubCloud
+ */
+package main
+import (
+	_ "github.com/ElvizLai/travisci/initial"
+	"github.com/astaxie/beego"
+)
+func main() {
+	beego.Run()
+}
