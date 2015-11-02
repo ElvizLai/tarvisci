33800446221c831f2d57bafecdd2e64a9db41b53
ElvizLai <sdrzlyz@126.com>
2015-11-02 15:32:09 +0800

    add test file

---
+++ initial/initial_test.go
@@ -0,0 +1,13 @@
+/**
+ * Created by elvizlai on 2015/11/2 15:29
+ * Copyright © PubCloud
+ */
+package initial_test
+import (
+	"testing"
+	"fmt"
+)
+
+func Test(t *testing.T) {
+	fmt.Println("pass")
+}
